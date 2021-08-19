c9226e2272050d7902128e30c50860861f391936
Loreen-netizen <loreenmuusha@gmail.com>
2021-08-19 14:22:54 +0200

    added functionality for active covid cases

--- components/ProvincialStatsCards/ProvincialStatsCards.jsx
+++ components/ProvincialStatsCards/ProvincialStatsCards.jsx
@@ -1,23 +1,18 @@
 import styles from "../../styles/ProvincialStatsCards.module.css";
-import { getDataByDate } from "../../services/covidService";
-import data from "../../data/covidData";
+import { nationalCasesToday } from "../../services/covidService";
+
 
 export const ProvincialStatsCards = () => {
-  const rsa = data["RSA"];
-  const nationalData = rsa["National"];
-  console.log(nationalData, "dddddddddddddddddddd");
-  const todaysCases = getDataByDate(new Date());
-  console.log(todaysCases,"llllllllll")
+ const nationalCasesCount = nationalCasesToday()
+
+ 
   return (
     <div>
       <div className={styles.grid}>
         <a href="https://nextjs.org/docs" className={styles.card}>
           <h2>National Covid Statistics Today </h2>
           <h1>
-            {Object.keys(nationalData)[0]} = {[todaysCases][0].reduce((total, item)=>{
-
-                return total + parseInt(item.cases)
-            },0)}
+           {nationalCasesCount}
           </h1>
         </a>
       </div>
--- services/covidService.js
+++ services/covidService.js
@@ -31,6 +31,11 @@ export const getDataByDate = (timestamp) => {
     province: "GP",
     cases: gp["Cases"][dateIndex],
     recoveries: gp["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      gp["Cases"][dateIndex],
+      gp["Recoveries"][dateIndex],
+      gp["Deaths"][dateIndex]
+    ),
     deaths: gp["Deaths"][dateIndex],
   };
   const westernCape = {
@@ -39,6 +44,11 @@ export const getDataByDate = (timestamp) => {
     province: "WC",
     cases: wc["Cases"][dateIndex],
     recoveries: wc["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      wc["Cases"][dateIndex],
+      wc["Recoveries"][dateIndex],
+      wc["Deaths"][dateIndex]
+    ),
     deaths: wc["Deaths"][dateIndex],
   };
   const kwazuluNatal = {
@@ -47,6 +57,11 @@ export const getDataByDate = (timestamp) => {
     province: "KZN",
     cases: kzn["Cases"][dateIndex],
     recoveries: kzn["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      kzn["Cases"][dateIndex],
+      kzn["Recoveries"][dateIndex],
+      kzn["Deaths"][dateIndex]
+    ),
     deaths: kzn["Deaths"][dateIndex],
   };
 
@@ -56,6 +71,11 @@ export const getDataByDate = (timestamp) => {
     province: "EC",
     cases: ec["Cases"][dateIndex],
     recoveries: ec["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      ec["Cases"][dateIndex],
+      ec["Recoveries"][dateIndex],
+      ec["Deaths"][dateIndex]
+    ),
     deaths: ec["Deaths"][dateIndex],
   };
   const freeState = {
@@ -64,6 +84,11 @@ export const getDataByDate = (timestamp) => {
     province: "FS",
     cases: fs["Cases"][dateIndex],
     recoveries: fs["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      fs["Cases"][dateIndex],
+      fs["Recoveries"][dateIndex],
+      fs["Deaths"][dateIndex]
+    ),
     deaths: fs["Deaths"][dateIndex],
   };
   const mpumalanga = {
@@ -72,35 +97,54 @@ export const getDataByDate = (timestamp) => {
     province: "MP",
     cases: mp["Cases"][dateIndex],
     recoveries: mp["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      mp["Cases"][dateIndex],
+      mp["Recoveries"][dateIndex],
+      mp["Deaths"][dateIndex]
+    ),
     deaths: mp["Deaths"][dateIndex],
   };
 
-   const limpopo = {
-     id: 7,
-     date: rsa["Dates"][dateIndex],
-     province: "LP",
-     cases: lp["Cases"][dateIndex],
-     recoveries: lp["Recoveries"][dateIndex],
-     deaths: lp["Deaths"][dateIndex],
-   };
+  const limpopo = {
+    id: 7,
+    date: rsa["Dates"][dateIndex],
+    province: "LP",
+    cases: lp["Cases"][dateIndex],
+    recoveries: lp["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      lp["Cases"][dateIndex],
+      lp["Recoveries"][dateIndex],
+      lp["Deaths"][dateIndex]
+    ),
+    deaths: lp["Deaths"][dateIndex],
+  };
 
-   const northWest = {
-     id: 8,
-     date: rsa["Dates"][dateIndex],
-     province: "NW",
-     cases: nw["Cases"][dateIndex],
-     recoveries: nw["Recoveries"][dateIndex],
-     deaths: nw["Deaths"][dateIndex],
-   };
-   const northenCape = {
-     id: 9,
-     date: rsa["Dates"][dateIndex],
-     province: "NC",
-     cases: nc["Cases"][dateIndex],
-     recoveries: nc["Recoveries"][dateIndex],
-     deaths: nc["Deaths"][dateIndex],
-   };
-
+  const northWest = {
+    id: 8,
+    date: rsa["Dates"][dateIndex],
+    province: "NW",
+    cases: nw["Cases"][dateIndex],
+    recoveries: nw["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      nw["Cases"][dateIndex],
+      nw["Recoveries"][dateIndex],
+      nw["Deaths"][dateIndex]
+    ),
+    deaths: nw["Deaths"][dateIndex],
+  };
+  const northenCape = {
+    id: 9,
+    date: rsa["Dates"][dateIndex],
+    province: "NC",
+    cases: nc["Cases"][dateIndex],
+    recoveries: nc["Recoveries"][dateIndex],
+    activeCases: calculateActiveCases(
+      nc["Cases"][dateIndex],
+      nc["Recoveries"][dateIndex],
+      nc["Deaths"][dateIndex]
+    ),
+    deaths: nc["Deaths"][dateIndex],
+  };
 
   return [
     gauteng,
@@ -115,32 +159,60 @@ export const getDataByDate = (timestamp) => {
   ];
 };
 
-const columns = [
-  { field: "date", headerName: "Date", width: 150 },
-  {
-    field: "province",
-    headerName: "Province",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "cases",
-    headerName: "Cases",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "recoveries",
-    headerName: "Recoveries",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "deaths",
-    headerName: "Deaths",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 150,
-  },
-];
+export const columnData = () => {
+  const columns = [
+    { field: "date", headerName: "Date", width: 150 },
+    {
+      field: "province",
+      headerName: "Province",
+      width: 200,
+      sortable: false,
+    },
+    {
+      field: "cases",
+      headerName: "Cases",
+      width: 200,
+      sortable: false,
+    },
+    {
+      field: "recoveries",
+      headerName: "Recoveries",
+      type: "number",
+      width: 200,
+      sortable: false,
+    },
+    {
+      field: "activeCases",
+      headerName: "Active Cases",
+      type: "number",
+      width: 200,
+      sortable: false,
+    },
+    {
+      field: "deaths",
+      headerName: "Deaths",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 200,
+    },
+  ];
+  return columns;
+};
+
+export const nationalCasesToday = () => {
+  const rsa = data["RSA"];
+  const nationalData = rsa["National"];
+  const todaysCases = getDataByDate(new Date());
+
+  const numberOfNationalCases = ` ${Object.keys(nationalData)[0]} = ${[
+    todaysCases,
+  ][0].reduce((total, item) => {
+    return total + parseInt(item.cases);
+  }, 0)}`;
+  return numberOfNationalCases;
+};
+
+const calculateActiveCases = (cases, recoveries, deaths) => {
+  const activeCasesCount = cases - recoveries - deaths;
+  return activeCasesCount;
+};
